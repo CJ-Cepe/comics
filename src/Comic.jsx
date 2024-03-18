589bf12b041c77635ed9a48c826c10f5ceb2e8e1
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-19 01:06:19 +0800

    Feat: display item details

--- src/Comic.jsx
+++ src/Comic.jsx
@@ -3,11 +3,19 @@ import { useLocation } from "react-router-dom"
 function Comic ({}){
     const location = useLocation()
     const data = location.state;
-    console.log(data)
-    
+
+    const {title, description, prices, series: {name}, thumbnail: {path, extension}} = data
+    const price = prices[0].price
+    const aspectRatio = "/" + "detail";
+    const imageSrc = `${path}${aspectRatio}.${extension}`
+
     return (
         <>
-            <div>yeah!</div>
+            <h1>{"TITLE: " + title}</h1>
+            <h2>{"SERIES: " + name}</h2>
+            <p>{"DESCRIPTION: " + description}</p>
+            <p>{"PRICE: " + price}</p>
+            <img src={imageSrc} alt={title} width={400}/>
         </>
     )
 }
