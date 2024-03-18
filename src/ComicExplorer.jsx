f394aa7411d2d59e0b20525535edb1e90dd8db91
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-18 20:36:20 +0800

    Feat: implement search

--- src/ComicExplorer.jsx
+++ src/ComicExplorer.jsx
@@ -5,18 +5,30 @@ import Cards from './Cards';
 
 function ComicExplorer(){
     const [comicList, setComicList] = useState(null)
+    const [searchValue, setSearchValue] = useState("")
+    const [searchTerm, setSearchTerm] = useState("wolverine")
 
     useEffect(()=>{
         const fetchData = async () => {
-            const result = await fetchContent();
+            const result = await fetchContent(searchTerm);
             setComicList(result);
         }
         fetchData();
-    }, [])
+    }, [searchTerm])
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value)
+    }
+
+    const handleSearchClick = () => {
+        console.log('blade')
+        setSearchTerm(searchValue)
+    }
 
     return (
         <>
-            <input type="text" />
+            <input type="text" value={searchValue} onChange={handleInputChange}/>
+            <button onClick={handleSearchClick}>Search</button>
             {!comicList ? (
                 <div>
                     Loading...
